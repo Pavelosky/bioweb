4429dd2f2d267447b51e6a9d0ca532cf311e9e39
Pavelosky <pawel.bialkowskigru@gmail.com>
2024-12-03 22:22:08 +0100

    added styling

--- genedata/views.py
+++ genedata/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
-    master_genes = Gene.objects.all()  
+    master_genes = Gene.objects.all()
     return render(request, 'genedata/index.html', {'master_genes': master_genes})
 
 def gene(request, pk): 
@@ -13,19 +13,19 @@ def gene(request, pk):
     print("Gene record:", pk, "access count:", str(gene.access))
     gene.save()
     master_genes = Gene.objects.all()
-    return render(request, 'genedata/gene.html', {'gene': gene, master_genes: 'master_genes'}) 
+    return render(request, 'genedata/gene.html', {'gene': gene, 'master_genes': master_genes}) 
 
 def list(request, type):
     genes = Gene.objects.filter(entity__exact=type)
-    master_genes = Gene.objects.all()   
-    return render(request, 'genedata/list.html', {'genes': genes, 'type': type, master_genes: 'master_genes'}) 
+    master_genes = Gene.objects.all()
+    return render(request, 'genedata/list.html', {'genes': genes, 'type': type, 'master_genes': master_genes}) 
 
 def poslist(request): 
     genes = Gene.objects.filter(entity__exact='Chromosome').filter(sense__startswith='+') 
-    master_genes = Gene.objects.all()   
-    return render(request, 'genedata/list.html', {'genes': genes, 'type': 'PosList', master_genes: 'master_genes'})
+    master_genes = Gene.objects.all()
+    return render(request, 'genedata/list.html', {'genes': genes, 'type': 'PosList', 'master_genes': master_genes})
 
 def delete(request, pk): 
     GeneAttributeLink.objects.filter(gene_id=pk).delete() 
-    Gene.objects.filter(pk=pk).delete() 
+    Gene.objects.filter(pk=pk).delete()
     return HttpResponseRedirect("/")  # Redirect to index page
