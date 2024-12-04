42c77501145bbbbeec0ccb9a1b62a46085a34271
Pavelosky <pawel.bialkowskigru@gmail.com>
2024-12-04 20:45:19 +0100

    added forms and validation

--- genedata/views.py
+++ genedata/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import *
 from django.http import HttpResponseRedirect 
+from .forms import *
 
 # Create your views here.
 def index(request):
@@ -29,3 +30,33 @@ def delete(request, pk):
     GeneAttributeLink.objects.filter(gene_id=pk).delete() 
     Gene.objects.filter(pk=pk).delete()
     return HttpResponseRedirect("/")  # Redirect to index page
+
+def create_ec(request): 
+    master_genes = Gene.objects.all() 
+    if request.method == 'POST': 
+        form = ECForm(request.POST) 
+        if form.is_valid(): 
+            ec = EC() 
+            ec.ec_name = form.cleaned_data['ec_name'] 
+            ec.save() 
+            return HttpResponseRedirect('/create_ec/') 
+        else:
+            return render(request, 'genedata/ec.html', {'error':"failed", 'form': form, 'master_genes': master_genes})
+    else: 
+        ecs = EC.objects.all() 
+        form = ECForm() 
+        return render(request, 'genedata/ec.html', {'form': form, 'ecs': ecs, 'master_genes': master_genes})
+    
+def create_gene(request): 
+    master_genes = Gene.objects.all()
+    if request.method == 'POST': 
+        form = GeneForm(request.POST)
+        if form.is_valid(): 
+            gene = form.save()
+            return HttpResponseRedirect('/create_gene/')
+        else: 
+            return render(request, 'genedata/create_gene.html', {'error':"failed", 'form': form, 'master_genes': master_genes})
+    else: 
+        form = GeneForm()
+        master_genes = Gene.objects.all()  
+    return render(request, 'genedata/create_gene.html', {'form': form, 'master_genes': master_genes})
